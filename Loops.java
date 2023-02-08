bf67e950f978662ce917b942a51fd7ae364b55e2
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2023-02-09 00:03:45 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,32 @@
+class Loops 
+{
+	public static void main(String[] args) 
+	{
+		// while loop
+		int n = 1;
+			System.out.println("While Loop");
+
+		while(n<=5){
+			System.out.println(n);
+			n++;
+		}
+
+		System.out.println("Do While Loop");
+
+		// do-while loop
+		int m = 1;
+		do{
+			System.out.println(m);
+			m++;
+	}
+	while(m<=5);
+
+		System.out.println("For Loop");
+
+		// for loop
+		for(int i=5; i>=1; i--){
+			System.out.println(i);
+		}
+
+}
+}
